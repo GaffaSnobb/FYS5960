26848e2d046b87f3d85c1e8764ec6f6a14bb4ad3
Jon Dahl <jon@dira.no>
2021-03-03 14:53:54 +0100

    I think the level density miscalculation is solved. At the first try, I iterated over all levels and checked how many levels were in the bin_size vicinity of the i'th level. That leads to levels being counted several times. Now I count how many levels are in the interval [0, bin_size), then move on to the interval [bin_size, 2*bin_size), etc.

--- exercises/recreate_fig_6/src/recreate_fig_6.py
+++ exercises/recreate_fig_6/src/recreate_fig_6.py
@@ -1,6 +1,8 @@
+import sys
 import numpy as np
+import matplotlib.pyplot as plt
 
-def level_density(bin_size):
+def level_density_lol(bin_size):
     """
     Read level data from file.  Calculate the level density for input
     bin size.
@@ -21,7 +23,7 @@ def level_density(bin_size):
     levels : numpy.ndarray
         The raw level data.
     """
-    fname = ""
+    fname = "levels_56Fe.txt"
     levels = np.loadtxt(fname)
     n_levels = len(levels)
     counts = np.zeros(n_levels)
@@ -34,7 +36,59 @@ def level_density(bin_size):
     return counts, counts/bin_size, levels
 
 
-def read_kshell_output(fname="summary_S24_usda.txt"):
+def level_density(levels, bin_size):
+    """
+    Calculate the level density for a given bin size.
+
+    Parameters
+    ----------
+    levels : numpy.ndarray
+        Energy levels.
+
+    bin_size : int, float
+        The interval of which to calculate the density.
+
+    Returns
+    -------
+    counts/bin_sizes : numpy.ndarray
+        The level density.
+    """
+    N = len(levels)
+    counts = []
+    bin_levels = []
+    current_bin = 0  # Initial.
+    # for i in range(N):
+    #     for j in range(N):
+    #         if (levels[j] >= levels[i] - bin_size/2) and (levels[j] <= levels[i] + bin_size/2):
+    #             counts[i] += 1
+
+    while current_bin < levels[-1]:
+        bin_levels.append(current_bin)
+        counts.append(np.sum(  current_bin < levels[levels < current_bin + bin_size]  ))
+        current_bin += bin_size
+
+    counts = np.array(counts)
+    return counts/bin_size, np.array(bin_levels)
+
+
+def read_kshell_output(fname):
+    """
+    Read energy level data, transition probabilities and transition
+    strengths from KSHELL output files.
+
+    Parameters
+    ----------
+    fname : string
+        Filename of KSHELL output file.
+
+    Returns
+    -------
+    E_x : list
+        List of energy levels.
+
+    B_M1 : list
+        Nested list, [E_x, b_m1, E_gamma].
+    """
 
     E_x = []    
     B_M1 = []
@@ -46,6 +100,9 @@ def read_kshell_output(fname="summary_S24_usda.txt"):
                 tmp = line.split()
                 E_x.append(float(tmp[6]))
             except IndexError:
+                """
+                End of energies.
+                """
                 break
 
     def load_m1_probabilities(infile):
@@ -57,25 +114,41 @@ def read_kshell_output(fname="summary_S24_usda.txt"):
                     """
                     Example: 2+(11) 18.393 2+(10) 17.791 0.602 0.1(  0.0) 0.1( 0.0)
                     """
-                    B_M1.append([float(tmp[1]), float(tmp[5][:-1])])
+                    E_gamma = abs(float(tmp[3]) - float(tmp[1]))
+                    E_i = float(tmp[1])
+                    b_m1 = float(tmp[5][:-1])
+                    B_M1.append([E_i, b_m1, E_gamma])
 
                 elif (tmp[1][-1] != ")") and (tmp[3][-1] == ")"):
                     """
                     Example: 2+(10) 17.791 2+( 1) 5.172 12.619 0.006( 0.00) 0.006( 0.00)
                     """
-                    B_M1.append([float(tmp[1]), float(tmp[6][:-1])])
+                    E_gamma = abs(float(tmp[4]) - float(tmp[1]))
+                    E_i = float(tmp[1])
+                    b_m1 = float(tmp[6][:-1])
+                    B_M1.append([E_i, b_m1, E_gamma])
                 
                 elif (tmp[1][-1] == ")") and (tmp[4][-1] != ")"):
                     """
                     Example: 3+( 8) 19.503 2+(11) 18.393 1.111 0.000( 0.00) 0.000( 0.00)
                     """
-                    B_M1.append([float(tmp[2]), float(tmp[6][:-1])])
+                    E_gamma = abs(float(tmp[4]) - float(tmp[2]))
+                    E_i = float(tmp[2])
+                    b_m1 = float(tmp[6][:-1])
+                    B_M1.append([E_i, b_m1, E_gamma])
 
                 elif (tmp[1][-1] == ")") and (tmp[4][-1] == ")"):
                     """
                     Example: 1+( 7) 19.408 2+( 9) 16.111 3.297 0.005( 0.00) 0.003( 0.00)
                     """
-                    B_M1.append([float(tmp[2]), float(tmp[7][:-1])])
+                    E_gamma = abs(float(tmp[5]) - float(tmp[2]))
+                    E_i = float(tmp[2])
+                    b_m1 = float(tmp[7][:-1])
+                    B_M1.append([E_i, b_m1, E_gamma])
+                else:
+                    print("WARNING: Structure not accounted for! Exiting...")
+                    print(line)
+                    sys.exit()
 
             except IndexError:
                 """
@@ -94,9 +167,10 @@ def read_kshell_output(fname="summary_S24_usda.txt"):
                 elif tmp[0] == "B(M1)":
                     load_m1_probabilities(infile)
             except IndexError:
+                """
+                Skip blank lines.
+                """
                 continue
-
-    
 
     return E_x, B_M1
 
@@ -113,7 +187,12 @@ def test_file_read():
     fname = "test_text_file.txt"
     E_x, B_M1 = read_kshell_output(fname)
     E_x_expected = [0.0, 8.016]
-    B_M1_expected = [[5.172, 20.5], [17.791, 0.0], [19.408, 5.7], [18.393, 0.1]]
+    B_M1_expected = [
+        [5.172, 20.5, 5.172],
+        [17.791, 0.0, 17.791],
+        [19.408, 5.7, 19.408 - 17.791],
+        [18.393, 0.1, 18.393 - 17.791]
+    ]
     
     for calculated, expected in zip(E_x, E_x_expected):
         msg = f"Error in E_x. Expected: {expected}, got: {calculated}."
@@ -121,12 +200,27 @@ def test_file_read():
 
     for calculated, expected in zip(B_M1, B_M1_expected):
         msg = f"Error in B_M1. Expected: {expected}, got: {calculated}."
-        success = (calculated[0] == expected[0]) and (calculated[1] == expected[1])
+        success = (calculated[0] == expected[0]) and (calculated[1] == expected[1]) and (calculated[2] == expected[2])
         assert success, msg
 
 
-
 if __name__ == "__main__":
-    # read_kshell_output()
     test_file_read()
+    # levels_1, B_M1 = read_kshell_output("kshell_output/summary_S24_usda.txt")
+    bin_size_1 = 0.2    # [MeV].
+    bin_size_2 = 0.5    # [MeV].
+
+    levels_2 = np.loadtxt("levels_56Fe.txt")
+    densities_1, bin_levels_1 = level_density(levels_2, bin_size_1*1000)
+    densities_2, bin_levels_2 = level_density(levels_2, bin_size_2*1000)
+    
+    plt.step(bin_levels_1, densities_1, label=f"{bin_size_1=}")
+    plt.step(bin_levels_2, densities_2, label=f"{bin_size_2=}")
+    plt.yscale("log")
+    plt.legend()
+    plt.savefig("level_density.png", dpi=300)
+    plt.show()
+
+
+
     pass
